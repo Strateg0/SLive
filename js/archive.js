340164de84cdd4ce325e4cf530c84fe117916e7e
Strateg0 <strategma6@gmail.com>
2025-07-15 10:18:39 +0300

    validation included

--- js/archive.js
+++ js/archive.js
@@ -3,4 +3,8 @@ function goToEditor() {
     // Directly go to the editor page
     window.location.href = 'editor.html';
   }
+  function goToArchive() {
+    // Directly go to the editor page
+    window.location.href = 'archive.html';
+  }
 
