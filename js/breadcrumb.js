eb8a2519b0949619d3058514013b6dc4d1d70291
Strateg0 <strategma6@gmail.com>
2025-08-12 17:49:58 +0300

    Delete images not needed others compressed

--- js/breadcrumb.js
+++ js/breadcrumb.js
@@ -1,18 +1,50 @@
 const translations = {
     en: {
-      'home': 'Home',
-      'sectors': 'Sectors',
-      'aboutUs': 'About Us',
-      'offer': 'What we offer',
-      'new': 'News',
-      'blog': 'Blog',
-      'contact': 'Contacts',
+     'home': 'Начало',
+      'sectors': 'Сектори',
+      'aboutUs': 'За нас',
+      'offer': 'Какво предлагаме',
+      'new': 'Новости',
+      'blog': 'Блог',
+      'contact': 'Контакт',
+      'structural_reform': 'Оценка на организационния модел на централната администрация на изпълнителната власт',
+      'NAMRB': 'Примерни модели за административни структури на седем групи общини и проекти на документи за организационното им развитие',
+      'consulting_services': 'Подобряване на ефективността и оптимизиране на структурата на администрацията на Министерството на външните работи',
+      'organizational': 'Организационна оценка и планиране на подобрения',
+      'analyze': 'Консолидиран анализ на организационното състояние на Министерство на отбраната',
+      'actual_standarts': 'Актуализирани стандарти за провеждане на граждански консултации',
+      'nfk': 'Анализ и оценка на въздействието на действащото законодателство за НФК',
+      'impact_assessment': 'Цялостна предварителна оценка на въздействието във връзка с регулиране на основни икономически сектори',
+      'preliminary': 'Цялостна предварителна оценка на въздействието във връзка с регулиране на основни икономически сектори',
+      'org_model': 'Нов организационен модел на НСОРБ',
+      'functional_analyze': 'Функционални анализи',
+      'action_plan': 'План за оценка на програма за морско дело, рибарство и аквакултури',
+      'elec_networks': 'Социалноикономически анализ и обобщен доклад с резултати от проект „Опазване на застрашени видове птици чрез обезопасяване на опасни за тях въздушни електропроводи в защитени зони от НАТУРА 2000 в Западна България/ LIFE BIRDS ON POWER LINES“',
+      'strategic': 'Последваща оценка за изпълнението на Националната стратегия за регионално развитие за периода 2012-2022 г.',
     },
     es: {
-      'home': 'Inicio',
-      'sectors': 'Sectores',
-      'about-us': 'Sobre Nosotros',
-      'contact': 'Contacto',
+     'home': 'Начало',
+      'sectors': 'Сектори',
+      'aboutUs': 'За нас',
+      'offer': 'Какво предлагаме',
+      'new': 'Новости',
+      'blog': 'Блог',
+      'contact': 'Контакт',
+      'structural_reform': 'Оценка на организационния модел на централната администрация на изпълнителната власт',
+      'NAMRB': 'Примерни модели за административни структури на седем групи общини и проекти на документи за организационното им развитие',
+      'consulting_services': 'Подобряване на ефективността и оптимизиране на структурата на администрацията на Министерството на външните работи',
+      'organizational': 'Организационна оценка и планиране на подобрения',
+      'analyze': 'Консолидиран анализ на организационното състояние на Министерство на отбраната',
+      'actual_standarts': 'Актуализирани стандарти за провеждане на граждански консултации',
+      'nfk': 'Анализ и оценка на въздействието на действащото законодателство за НФК',
+      'impact_assessment': 'Цялостна предварителна оценка на въздействието във връзка с регулиране на основни икономически сектори',
+      'preliminary': 'Цялостна предварителна оценка на въздействието във връзка с регулиране на основни икономически сектори',
+      'org_model': 'Нов организационен модел на НСОРБ',
+      'functional_analyze': 'Функционални анализи',
+      'action_plan': 'План за оценка на програма за морско дело, рибарство и аквакултури',
+      'elec_networks': 'Социалноикономически анализ и обобщен доклад с резултати от проект „Опазване на застрашени видове птици чрез обезопасяване на опасни за тях въздушни електропроводи в защитени зони от НАТУРА 2000 в Западна България/ LIFE BIRDS ON POWER LINES“',
+      'strategic': 'Последваща оценка за изпълнението на Националната стратегия за регионално развитие за периода 2012-2022 г.',
+      'reg_plans': 'Последваща оценка за изпълнението на Националната стратегия за регионално развитие за периода 2012-2022 г.',
     },
     bg: {
       'home': 'Начало',
@@ -34,8 +66,10 @@ const translations = {
       'org_model': 'Нов организационен модел на НСОРБ',
       'functional_analyze': 'Функционални анализи',
       'action_plan': 'План за оценка на програма за морско дело, рибарство и аквакултури',
-      'elec_networks': 'Електроразпределителни мрежи Запад ЕАД',
-      'strategic': 'Министерство на регионалното развитие и благоустройството',
+      'elec_networks': 'Социалноикономически анализ и обобщен доклад с резултати от проект „Опазване на застрашени видове птици чрез обезопасяване на опасни за тях въздушни електропроводи в защитени зони от НАТУРА 2000 в Западна България/ LIFE BIRDS ON POWER LINES“',
+      'strategic': 'Последваща оценка за изпълнението на националната стратегия за регионално развитие за периода 2012-2022 г.',
+      'reg_plans': 'Последващи оценки на регионалните планове за развитие (РПР) 2014 – 2020 г.',
+      'integrated_dev': 'Планове за интегрирано развитие на общини за периода 2021-2027',
     }
   };
 
@@ -63,11 +97,20 @@ const translations = {
     'action_plan.html': ['home', 'offer', 'action_plan'],
     'elec_networks.html': ['home', 'sectors', 'elec_networks'],
     'strategic.html': ['home', 'offer', 'strategic'],
+    'reg_plans.html': ['home', 'offer', 'reg_plans'],
+    'integrated_dev.html': ['home', 'offer', 'integrated_dev'],
   };
+  
+// Shorten text function
+// function shortenText(text, maxLength = 100) {
+//   if (text.length > maxLength) {
+//     return text.slice(0, maxLength - 5) + '…';
+//   }
+//   return text;
+// }
 
   const currentPath = window.location.pathname.split('/').pop();
   const trail = breadcrumbMap[currentPath];
-
   const breadcrumb = document.getElementById('breadcrumb');
   let html = `<ol class="breadcrumb-list">`; 
 
@@ -75,6 +118,7 @@ const translations = {
     trail.forEach((key, index) => {
       const label = translations[lang][key] || key;
       const isLast = index === trail.length - 1;
+      // const shortened = shortenText(label, 100);
 
       const hrefMap = {
         home: 'index.html',
@@ -93,16 +137,20 @@ const translations = {
         functional_analyze: 'org_model.html',
         action_plan: 'action_plan.html',
         elec_networks: 'elec_networks.html',
-        strategic: 'strategic.html'
+        strategic: 'strategic.html',
+        reg_plans: 'reg_plans.html',
+        integrated_dev: 'integrated_dev.html'
       };
 
       if (isLast) {
-        html += `<li aria-current="page">${label}</li>`;
+         html += `<li aria-current="page" class="truncate" title="${label}">${label}</li>`;
       } else {
-        html += `<li><a href="${hrefMap[key]}">${label}</a></li>`;
+      html += `<li><a href="${hrefMap[key]}" class="truncate" title="${label}">${label}</a></li>`;
       }
     });
   }
 
   html += `</ol>`;
   breadcrumb.innerHTML = html;
+
+  //---END 
